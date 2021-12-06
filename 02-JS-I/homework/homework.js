677d713792c49c84126be6b8d1b2536aaf79c686
abisol-2711 <solabigail00@gmail.com>
2021-12-06 13:02:39 -0300

    Eliminacion de variable

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -226,7 +226,6 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  var dolar = 1.20;
   var cambioADolar = euro * 1.20;
   return cambioADolar;  
 }
